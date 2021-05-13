750a6ab00ffd5dfa3ea382b4bd35fa987e04859c
Juan Macarlupu <juanmacarlupu@gmail.com>
2021-05-13 17:01:20 -0300

    Entrega clase 3

---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const BarraApp = ()=> {
+const classes = useStyles();
+
+    return (
+        <AppBar position="static" style={{background: "#e6b960"}}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            Juan Macarlupu - Capacitación para traductores
+          </Typography>
+          <Button color="inherit">Cursos gratuitos</Button>
+          <Button color="inherit">Cursos intensivos</Button>
+          <Button color="inherit">Trayecto de formación</Button>
+          <Button color="inherit">Cursos regulares</Button>
+        </Toolbar>
+      </AppBar>
+    )
+};
+
+export default BarraApp;
